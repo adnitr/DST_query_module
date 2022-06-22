77b664dd8a0378c6b3864fcaf0af62f0f1ee1b98
Ashutosh Panda <ashutoshpandams@gmail.com>
2022-06-22 20:12:54 +0530

    back and forward button now working, new query page added
    need css edit for new query page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import home from "./components/home";
 import querypage from "./components/querypage";
 import department1 from "./components/DEPARTMENTS/department_1";
 import department2 from "./components/DEPARTMENTS/department_2";
+import new_query from "./new";
+window.onpopstate = function(event) {
+  window.location.reload(false);
+};
 function App() {
     return (
     <>
@@ -14,6 +18,7 @@ function App() {
           <Route exact path="/querypage" component={querypage}/>
           <Route exact path="/querypage/department_1" component={department1}/>
           <Route exact path="/querypage/department_2" component={department2}/>
+          <Route exact path="/new" component={new_query}/>
           <Redirect to ="/"/>
         </Switch>
       </Router >
